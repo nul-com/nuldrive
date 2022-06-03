0423aa8a2752ef8ba4569ec7781a01495918e5d5
erias04 <github.eliascerne@icloud.com>
2022-06-03 14:21:54 +0200

    create: new page quick.tsx

---
+++ apps/web/pages/quick.tsx
@@ -0,0 +1,14 @@
+
+
+/* eslint-disable-next-line */
+export interface QuickProps {}
+
+export function Quick(props: QuickProps) {
+  return (
+    <div className="">
+      <h1>Welcome to Quick!</h1>
+    </div>
+  );
+}
+
+export default Quick;
