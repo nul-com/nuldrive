9c4c68da996330bbb4362dfe9706a9902360afd6
erias04 <github.eliascerne@icloud.com>
2022-06-04 18:26:47 +0200

    feat: added naviagtion modal

--- libs/web/layouts/base/header/src/lib/WebLayoutsBaseHeader.tsx
+++ libs/web/layouts/base/header/src/lib/WebLayoutsBaseHeader.tsx
@@ -1,7 +1,10 @@
 import Link from 'next/link';
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react';
 import { Logo } from '@nuldrive/web/util/icons';
 import { CloudUpload } from '@nuldrive/web/util/icons';
 import { Apps } from '@nuldrive/web/util/icons';
+import { WebLayoutsBaseNavigation } from '@nuldrive/web/layouts/base/navigation';
 
 import styles from './WebLayoutsBaseHeader.module.css';
 
@@ -9,6 +12,7 @@ import styles from './WebLayoutsBaseHeader.module.css';
 export interface WebLayoutsBaseHeaderProps {}
 
 export function WebLayoutsBaseHeader(props: WebLayoutsBaseHeaderProps) {
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
   return (
     <header className="fixed w-screen bottom-0 left-0 z-40 bg-primaryColor shadow-2xl shadow-searchBarBg">
       <nav className="max-w-header ml-6 mr-5 h-header flex justify-between items-center text-black">
@@ -29,16 +33,41 @@ export function WebLayoutsBaseHeader(props: WebLayoutsBaseHeaderProps) {
               Open Chat
             </div>
           </Link>
-          <div className="w-5">
+          <div className="w-5" onClick={() => setNavMenuOpen(true)}>
             <Apps />
           </div>
         </div>
       </nav>
-      {/* <NavMenu
-        navMenuOpen={navMenuOpen}
-        setNavMenuOpen={setNavMenuOpen}
-        setIsSearchOpen={setIsSearchOpen}
-      /> */}
+
+      <Transition appear show={navMenuOpen} as={Fragment}>
+        <Dialog as="div" className="" onClose={() => setNavMenuOpen(false)}>
+          {/* <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="" />
+          </Transition.Child> */}
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-200"
+            enterFrom="-bottom-40"
+            enterTo="bottom-0"
+            leave="ease-in duration-200"
+            leaveFrom="bottom-0"
+            leaveTo="-bottom-40"
+          >
+            <div className="fixed bottom-0">
+              <WebLayoutsBaseNavigation />
+            </div>
+          </Transition.Child>
+        </Dialog>
+      </Transition>
     </header>
   );
 }
