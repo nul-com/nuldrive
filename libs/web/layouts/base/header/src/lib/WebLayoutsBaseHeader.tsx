c17a62d95f16cc4d527a84de2377777afd7caf48
erias04 <github.eliascerne@icloud.com>
2022-06-04 14:35:53 +0200

    feat: append Logo to the header

--- libs/web/layouts/base/header/src/lib/WebLayoutsBaseHeader.tsx
+++ libs/web/layouts/base/header/src/lib/WebLayoutsBaseHeader.tsx
@@ -1,3 +1,5 @@
+import { Logo } from '@nuldrive/web/util/icons';
+
 import styles from './WebLayoutsBaseHeader.module.css';
 
 /* eslint-disable-next-line */
@@ -6,6 +8,7 @@ export interface WebLayoutsBaseHeaderProps {}
 export function WebLayoutsBaseHeader(props: WebLayoutsBaseHeaderProps) {
   return (
     <div className="h-header bg-white w-screen">
+      <Logo />
       <div className="">Hello world</div>
     </div>
   );
