d6e86dc0e09545934f6e74e228ffa3cd11358c2f
erias04 <github.eliascerne@icloud.com>
2022-06-04 14:29:52 +0200

    feat: added header to Layout

--- libs/web/layouts/base/ui/src/lib/WebLayoutsBaseUi.tsx
+++ libs/web/layouts/base/ui/src/lib/WebLayoutsBaseUi.tsx
@@ -1,4 +1,5 @@
 import { ReactChild } from 'react';
+import { WebLayoutsBaseHeader } from '@nuldrive/web/layouts/base/header';
 import styles from './WebLayoutsBaseUi.module.css';
 
 /* eslint-disable-next-line */
@@ -8,7 +9,14 @@ export interface WebLayoutsBaseUiProps {
 
 export function WebLayoutsBaseUi(props: WebLayoutsBaseUiProps) {
   const { children } = props;
-  return <div className="">{children}</div>;
+  return (
+    <main className="">
+      <div className="fixed">
+        <WebLayoutsBaseHeader />
+      </div>
+      {children}
+    </main>
+  );
 }
 
 export default WebLayoutsBaseUi;
