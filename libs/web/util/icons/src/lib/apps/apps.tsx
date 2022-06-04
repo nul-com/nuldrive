71c87a402ec8cab2bd358c3da5c133b53abddf64
erias04 <github.eliascerne@icloud.com>
2022-06-04 18:39:06 +0200

    feat: changed props to any

--- libs/web/util/icons/src/lib/apps/apps.tsx
+++ libs/web/util/icons/src/lib/apps/apps.tsx
@@ -3,7 +3,7 @@ import styles from './apps.module.css';
 /* eslint-disable-next-line */
 export interface AppsProps {}
 
-export function Apps(props: AppsProps) {
+export function Apps(props: any) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
       <path
