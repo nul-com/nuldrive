4f9a84660887a1c0ed505f1422ce64ae23984ec0
erias04 <github.eliascerne@icloud.com>
2022-06-04 14:23:00 +0200

    feat: added new spacing header

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        header: '47px',
+      },
+    },
   },
   plugins: [],
 };
