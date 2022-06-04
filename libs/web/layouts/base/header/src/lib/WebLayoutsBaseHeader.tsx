291462f47638590af4c8008bd394fd5ea5418d1a
erias04 <github.eliascerne@icloud.com>
2022-06-04 18:03:42 +0200

    feat: added styling for mobile header

--- libs/web/layouts/base/header/src/lib/WebLayoutsBaseHeader.tsx
+++ libs/web/layouts/base/header/src/lib/WebLayoutsBaseHeader.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import { Logo } from '@nuldrive/web/util/icons';
 import { CloudUpload } from '@nuldrive/web/util/icons';
+import { Apps } from '@nuldrive/web/util/icons';
 
 import styles from './WebLayoutsBaseHeader.module.css';
 
@@ -8,15 +10,36 @@ export interface WebLayoutsBaseHeaderProps {}
 
 export function WebLayoutsBaseHeader(props: WebLayoutsBaseHeaderProps) {
   return (
-    <div className="flex items-center justify-between px-3 h-header bg-blue-300 w-screen">
-      <div className="w-9">
-        <Logo />
-      </div>
-      <div className="text-black w-6">
-        <CloudUpload width={100} />
-      </div>
-      <div className="">Hello world</div>
-    </div>
+    <header className="fixed w-screen bottom-0 left-0 z-40 bg-primaryColor shadow-2xl shadow-searchBarBg">
+      <nav className="max-w-header ml-6 mr-5 h-header flex justify-between items-center text-black">
+        <Link href="/">
+          <div className="cursor-pointer flex gap-1.5 items-center">
+            <div className="w-7.5">
+              <Logo />
+            </div>
+            <div className="flex gap-1">
+              <h1 className="font-extrabold text-lg">nuldrive</h1>
+              <p className="text-xxs text-countryCode font-medium">AT</p>
+            </div>
+          </div>
+        </Link>
+        <div className="flex items-center cursor-pointer">
+          <Link href="/">
+            <div className="px-2.5 py-1 border-[1px] border-black rounded-3xl text-sm font-semibold mr-3">
+              Open Chat
+            </div>
+          </Link>
+          <div className="w-5">
+            <Apps />
+          </div>
+        </div>
+      </nav>
+      {/* <NavMenu
+        navMenuOpen={navMenuOpen}
+        setNavMenuOpen={setNavMenuOpen}
+        setIsSearchOpen={setIsSearchOpen}
+      /> */}
+    </header>
   );
 }
 
