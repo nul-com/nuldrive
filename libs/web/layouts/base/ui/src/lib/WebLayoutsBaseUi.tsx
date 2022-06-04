9b785b972a7b8abb3f87cdcc2c8815c3efe5a899
erias04 <github.eliascerne@icloud.com>
2022-06-04 13:40:43 +0200

    feat: added children to Layout

--- libs/web/layouts/base/ui/src/lib/WebLayoutsBaseUi.tsx
+++ libs/web/layouts/base/ui/src/lib/WebLayoutsBaseUi.tsx
@@ -1,14 +1,14 @@
+import { ReactChild } from 'react';
 import styles from './WebLayoutsBaseUi.module.css';
 
 /* eslint-disable-next-line */
-export interface WebLayoutsBaseUiProps {}
+export interface WebLayoutsBaseUiProps {
+  children: ReactChild;
+}
 
 export function WebLayoutsBaseUi(props: WebLayoutsBaseUiProps) {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebLayoutsBaseUi!</h1>
-    </div>
-  );
+  const { children } = props;
+  return <div className="">{children}</div>;
 }
 
 export default WebLayoutsBaseUi;
