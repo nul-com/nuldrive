58f9037c34957abbbc22be4e1dbde1091674340d
erias04 <github.eliascerne@icloud.com>
2022-06-04 15:04:41 +0200

    feat: added export in icons

--- libs/web/util/icons/src/index.ts
+++ libs/web/util/icons/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/logo/logo';
+export * from './lib/cloud-upload/cloud-upload';
 export * from './lib/cloud-times/cloud-times';
 export * from './lib/cloud-download/cloud-download';
 export * from './lib/cloud-check/cloud-check';
