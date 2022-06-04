f700e1a29ed9774ce8951bb636f2cdb769ce5681
erias04 <github.eliascerne@icloud.com>
2022-06-04 18:40:13 +0200

    add: new icon times (x)

---
+++ libs/web/util/icons/src/lib/times/times.tsx
@@ -0,0 +1,17 @@
+import styles from './times.module.css';
+
+/* eslint-disable-next-line */
+export interface TimesProps {}
+
+export function Times(props: TimesProps) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="auto"
+        d="M13.41 12l4.3-4.29a1 1 0 10-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 00-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 000 1.42 1 1 0 001.42 0l4.29-4.3 4.29 4.3a1 1 0 001.42 0 1 1 0 000-1.42z"
+      />
+    </svg>
+  );
+}
+
+export default Times;
