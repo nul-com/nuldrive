55f44d3a301215ad6845969d9f8c004d9edc6a27
erias04 <github.eliascerne@icloud.com>
2022-06-04 15:04:32 +0200

    feat: added cloudUpload icon

--- libs/web/layouts/base/header/src/lib/WebLayoutsBaseHeader.tsx
+++ libs/web/layouts/base/header/src/lib/WebLayoutsBaseHeader.tsx
@@ -1,4 +1,5 @@
 import { Logo } from '@nuldrive/web/util/icons';
+import { CloudUpload } from '@nuldrive/web/util/icons';
 
 import styles from './WebLayoutsBaseHeader.module.css';
 
@@ -10,6 +11,9 @@ export function WebLayoutsBaseHeader(props: WebLayoutsBaseHeaderProps) {
     <div className="flex items-center justify-between px-3 h-header bg-blue-300 w-screen">
       <div className="w-9">
         <Logo />
+      </div>
+      <div className="text-black w-6">
+        <CloudUpload width={100} />
       </div>
       <div className="">Hello world</div>
     </div>
