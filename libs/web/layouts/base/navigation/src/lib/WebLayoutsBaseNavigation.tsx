da66165e8278dd52f82e14113494dbdb83f7016f
erias04 <github.eliascerne@icloud.com>
2022-06-04 18:38:49 +0200

    feat: changed icons imports

--- libs/web/layouts/base/navigation/src/lib/WebLayoutsBaseNavigation.tsx
+++ libs/web/layouts/base/navigation/src/lib/WebLayoutsBaseNavigation.tsx
@@ -1,11 +1,15 @@
+import { Bolt, Folder, User, Times } from '@nuldrive/web/util/icons';
 import Link from 'next/link';
 
 import styles from './WebLayoutsBaseNavigation.module.css';
 
 /* eslint-disable-next-line */
-export interface WebLayoutsBaseNavigationProps {}
+export interface WebLayoutsBaseNavigationProps {
+  setNavMenuOpen: any;
+}
 
 export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
+  const { setNavMenuOpen } = props;
   return (
     <div className="fixed bottom-0 left-0 w-full bg-primaryColor pt-8 px-6 pb-5 drop-shadow-3xl rounded-2xl border-4 border-gray-700 z-50">
       <div className="grid gap-6">
@@ -13,7 +17,9 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
           <li className="cursor-pointer">
             <Link href="/link">
               <div className="flex flex-col items-center text-sm font-medium">
-                {/* <FaCircleNotch className="text-xl" /> */}
+                <span className="w-5">
+                  <Bolt />
+                </span>
                 <p className="">About</p>
               </div>
             </Link>
@@ -21,7 +27,9 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
           <li className="cursor-pointer">
             <Link href="/link">
               <div className="flex flex-col items-center text-sm font-medium">
-                {/* <HiOutlineSupport className="text-xl" /> */}
+                <span className="w-5">
+                  <Folder />
+                </span>
                 <p className="">FAQ</p>
               </div>
             </Link>
@@ -29,8 +37,10 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
           <li className="cursor-pointer">
             <Link href="/link">
               <div className="flex flex-col items-center text-sm font-medium">
-                {/* <IoMdCheckmarkCircleOutline className="text-xl" /> */}
-                <p className="">Safety</p>
+                <span className="w-5">
+                  <User />
+                </span>
+                <p className="">User</p>
               </div>
             </Link>
           </li>
@@ -42,10 +52,12 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
               setNavMenuOpen={setNavMenuOpen}
             /> */}
           </div>
-          {/* <IoMdClose
-            className="text-2xl cursor-pointer text-titleColor mr-1"
-            onClick={closeModal}
-          /> */}
+          <span
+            className="w-6 cursor-pointer text-titleColor mr-1"
+            onClick={() => setNavMenuOpen(false)}
+          >
+            <Times />
+          </span>
         </div>
       </div>
     </div>
