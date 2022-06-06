08649d7638f9c7192d51f39d22a3b18fd9e76c63
erias04 <github.eliascerne@icloud.com>
2022-06-06 22:58:15 +0200

    create: new lib upgrade in web/util/buttons

---
+++ libs/web/util/buttons/upgrade/src/lib/WebUtilButtonsUpgrade.tsx
@@ -0,0 +1,14 @@
+import styles from './WebUtilButtonsUpgrade.module.css';
+
+/* eslint-disable-next-line */
+export interface WebUtilButtonsUpgradeProps {}
+
+export function WebUtilButtonsUpgrade(props: WebUtilButtonsUpgradeProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebUtilButtonsUpgrade!</h1>
+    </div>
+  );
+}
+
+export default WebUtilButtonsUpgrade;
