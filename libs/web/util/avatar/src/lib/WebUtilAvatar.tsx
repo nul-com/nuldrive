117d058b495a1622ed7aa34ac6d5b7851c5abd96
erias04 <github.eliascerne@icloud.com>
2022-06-06 22:26:41 +0200

    feat: added avatar image loader This component will change and a new lib imageLoader replaces the image Loader part

--- libs/web/util/avatar/src/lib/WebUtilAvatar.tsx
+++ libs/web/util/avatar/src/lib/WebUtilAvatar.tsx
@@ -1,13 +1,38 @@
+import Image from 'next/image';
+
 import styles from './WebUtilAvatar.module.css';
 
 /* eslint-disable-next-line */
-export interface WebUtilAvatarProps {}
+export interface WebUtilAvatarProps {
+  src: string;
+  width: number;
+  height?: number;
+  quality?: string;
+  alt?: string;
+}
+
+interface IimageLoader {
+  src: string | undefined;
+  width: number | undefined;
+  quality?: number | undefined;
+}
+
+const imageLoader = (props: IimageLoader) => {
+  const { src, width, quality } = props;
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
 
 export function WebUtilAvatar(props: WebUtilAvatarProps) {
+  const { src, width, height, quality, alt } = props;
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebUtilAvatar!</h1>
-    </div>
+    <Image
+      loader={imageLoader}
+      src={src}
+      quality={quality}
+      alt={alt || 'user'}
+      width={width}
+      height={height || width}
+    />
   );
 }
 
