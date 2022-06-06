5502daa6d742a3b8bea41ef489ca095a792502ba
erias04 <github.eliascerne@icloud.com>
2022-06-06 22:02:40 +0200

    feat: export user-plus icon

--- libs/web/util/icons/src/index.ts
+++ libs/web/util/icons/src/index.ts
@@ -10,3 +10,4 @@ export * from './lib/user/user';
 export * from './lib/times/times';
 export * from './lib/search/search';
 export * from './lib/command/command';
+export * from './lib/user-plus/user-plus';
