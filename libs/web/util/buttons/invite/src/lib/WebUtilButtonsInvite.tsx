5a99e62610a8636057c312e362fbb9b69cd1b15f
erias04 <github.eliascerne@icloud.com>
2022-06-06 21:46:32 +0200

    create: new lib invite in util/buttons

---
+++ libs/web/util/buttons/invite/src/lib/WebUtilButtonsInvite.tsx
@@ -0,0 +1,14 @@
+import styles from './WebUtilButtonsInvite.module.css';
+
+/* eslint-disable-next-line */
+export interface WebUtilButtonsInviteProps {}
+
+export function WebUtilButtonsInvite(props: WebUtilButtonsInviteProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebUtilButtonsInvite!</h1>
+    </div>
+  );
+}
+
+export default WebUtilButtonsInvite;
