f35a51f20c0841f60492df5d74e811be1437c46f
erias04 <github.eliascerne@icloud.com>
2022-06-06 22:51:05 +0200

    feat: changed lib name 'imageLoader'

--- libs/web/util/imageloader/jest.config.js
+++ libs/web/util/imageloader/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'web-util-imageloader',
+  displayName: 'web-util-imageLoader',
   preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/web/util/imageloader',
+  coverageDirectory: '../../../../coverage/libs/web/util/imageLoader',
 };
