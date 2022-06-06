c27155f0b3398438664f8f5645ea7ab70bdf8feb
erias04 <github.eliascerne@icloud.com>
2022-06-06 23:42:48 +0200

    feat: styled upgrade button

--- libs/web/util/buttons/upgrade/src/lib/WebUtilButtonsUpgrade.tsx
+++ libs/web/util/buttons/upgrade/src/lib/WebUtilButtonsUpgrade.tsx
@@ -1,3 +1,4 @@
+import { AngleDown } from '@nuldrive/web/util/icons';
 import styles from './WebUtilButtonsUpgrade.module.css';
 
 /* eslint-disable-next-line */
@@ -5,8 +6,13 @@ export interface WebUtilButtonsUpgradeProps {}
 
 export function WebUtilButtonsUpgrade(props: WebUtilButtonsUpgradeProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebUtilButtonsUpgrade!</h1>
+    <div className="w-[105px] h-[27px] flex justify-between bg-[#F2F2F2] rounded-[4px]">
+      <div className="flex w-full self-center justify-center">
+        <p className="text-[11px] font-medium text-[#494949]">Upgrade</p>
+      </div>
+      <div className="border-l-[1px] border-[#E1E1E1] self-center">
+        <AngleDown width={21} fill="#6B6B6B" />
+      </div>
     </div>
   );
 }
