58704b9dde15278c42bdfd33f761a4400c5aa1e1
erias04 <github.eliascerne@icloud.com>
2022-06-07 00:15:14 +0200

    create: new lib bucketSelect in web/util/buttons

---
+++ libs/web/util/buttons/bucketselect/src/lib/WebUtilButtonsBucketSelect.tsx
@@ -0,0 +1,16 @@
+import styles from './WebUtilButtonsBucketselect.module.css';
+
+/* eslint-disable-next-line */
+export interface WebUtilButtonsBucketselectProps {}
+
+export function WebUtilButtonsBucketselect(
+  props: WebUtilButtonsBucketselectProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebUtilButtonsBucketselect!</h1>
+    </div>
+  );
+}
+
+export default WebUtilButtonsBucketselect;
