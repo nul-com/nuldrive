ec0ff36c52dd5dd96d97a164ec83d069fdc8cef7
erias04 <github.eliascerne@icloud.com>
2022-06-07 00:09:55 +0200

    feat: styled upgrade button

--- libs/web/util/buttons/upgrade/src/lib/WebUtilButtonsUpgrade.tsx
+++ libs/web/util/buttons/upgrade/src/lib/WebUtilButtonsUpgrade.tsx
@@ -6,11 +6,11 @@ export interface WebUtilButtonsUpgradeProps {}
 
 export function WebUtilButtonsUpgrade(props: WebUtilButtonsUpgradeProps) {
   return (
-    <div className="w-[105px] h-[27px] flex justify-between bg-[#F2F2F2] rounded-[4px]">
-      <div className="flex w-full self-center justify-center">
-        <p className="text-[11px] font-medium text-[#494949]">Upgrade</p>
+    <div className=" flex justify-between bg-[#F2F2F2] rounded-[4px]">
+      <div className="flex w-full items-center justify-center">
+        <p className="text-[11px] px-[16px] py-[5.5px] font-medium text-[#494949]">Upgrade</p>
       </div>
-      <div className="border-l-[1px] border-[#E1E1E1] self-center">
+      <div className="w-[23px] border-l-[1px] border-[#E1E1E1] self-center">
         <AngleDown width={21} fill="#6B6B6B" />
       </div>
     </div>
