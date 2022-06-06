c5495c959f91e86cf62f13bedb4136189e993333
erias04 <github.eliascerne@icloud.com>
2022-06-06 22:02:25 +0200

    feat: styled invite button

--- libs/web/util/buttons/invite/src/lib/WebUtilButtonsInvite.tsx
+++ libs/web/util/buttons/invite/src/lib/WebUtilButtonsInvite.tsx
@@ -1,3 +1,5 @@
+import { UserPlus } from '@nuldrive/web/util/icons';
+
 import styles from './WebUtilButtonsInvite.module.css';
 
 /* eslint-disable-next-line */
@@ -5,9 +7,12 @@ export interface WebUtilButtonsInviteProps {}
 
 export function WebUtilButtonsInvite(props: WebUtilButtonsInviteProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebUtilButtonsInvite!</h1>
-    </div>
+    <button className="w-[67px] h-[24px] bg-blue rounded-[6px] flex justify-center items-center gap-[5px]">
+      <div className="w-[17px] fill-white">
+        <UserPlus />
+      </div>
+      <div className="text-white text-[11px]">Invite</div>
+    </button>
   );
 }
 
