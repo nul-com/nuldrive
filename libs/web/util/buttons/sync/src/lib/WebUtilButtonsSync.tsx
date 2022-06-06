cac2ce8bd5659acde89e9bd624f50c04ba414c30
erias04 <github.eliascerne@icloud.com>
2022-06-06 21:45:13 +0200

    feat: styled and added content to sync button

--- libs/web/util/buttons/sync/src/lib/WebUtilButtonsSync.tsx
+++ libs/web/util/buttons/sync/src/lib/WebUtilButtonsSync.tsx
@@ -1,3 +1,5 @@
+import {CloudCheck} from '@nuldrive/web/util/icons'
+
 import styles from './WebUtilButtonsSync.module.css';
 
 /* eslint-disable-next-line */
@@ -5,8 +7,10 @@ export interface WebUtilButtonsSyncProps {}
 
 export function WebUtilButtonsSync(props: WebUtilButtonsSyncProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebUtilButtonsSync!</h1>
+    <div className="bg-[#EAFEF2] w-[30px] flex justify-center rounded-[11px] cursor-pointer">
+      <div className="w-[20px] fill-[#377D3B]">
+        <CloudCheck />
+      </div>
     </div>
   );
 }
