da69d00af6e23001065bfc5582260207242c74a9
erias04 <github.eliascerne@icloud.com>
2022-06-06 21:30:56 +0200

    feat: added tabs lib to the base layout

--- libs/web/layouts/base/ui/src/lib/WebLayoutsBaseUi.tsx
+++ libs/web/layouts/base/ui/src/lib/WebLayoutsBaseUi.tsx
@@ -3,6 +3,7 @@ import { WebLayoutsBaseHeader } from '@nuldrive/web/layouts/base/header';
 import styles from './WebLayoutsBaseUi.module.css';
 import { WebLayoutsBasePanels } from '@nuldrive/web/layouts/base/panels';
 import { WebLayoutsBaseFooter } from '@nuldrive/web/layouts/base/footer';
+import { WebLayoutsBaseTabs } from '@nuldrive/web/layouts/base/tabs';
 
 /* eslint-disable-next-line */
 export interface WebLayoutsBaseUiProps {
@@ -21,6 +22,7 @@ export function WebLayoutsBaseUi(props: WebLayoutsBaseUiProps) {
   return (
     <main className="flex flex-col h-screen w-screen overflow-hidden">
       <WebLayoutsBaseHeader />
+      <WebLayoutsBaseTabs />
       <WebLayoutsBasePanels children={children} />
       <WebLayoutsBaseFooter />
     </main>
