59bda829e92520a5b42b18804416c0f76773839a
erias04 <github.eliascerne@icloud.com>
2022-06-06 21:31:12 +0200

    feat: styled sync button

--- libs/web/util/sync/src/lib/WebUtilSync.tsx
+++ libs/web/util/sync/src/lib/WebUtilSync.tsx
@@ -1,3 +1,5 @@
+import { CloudCheck } from '@nuldrive/web/util/icons';
+
 import styles from './WebUtilSync.module.css';
 
 /* eslint-disable-next-line */
@@ -5,8 +7,10 @@ export interface WebUtilSyncProps {}
 
 export function WebUtilSync(props: WebUtilSyncProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebUtilSync!</h1>
+    <div className="bg-[#EAFEF2] w-[30px] flex justify-center rounded-[11px] cursor-pointer">
+      <div className="w-[20px] fill-[#377D3B]">
+        <CloudCheck />
+      </div>
     </div>
   );
 }
