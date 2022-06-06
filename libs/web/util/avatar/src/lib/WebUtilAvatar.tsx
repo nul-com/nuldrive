4427305e44813f2e61d3ce6c4b7c1684f1e9625d
erias04 <github.eliascerne@icloud.com>
2022-06-06 22:52:04 +0200

    feat: moved image Loader to avatar This acts now as a handler (middleware)

--- libs/web/util/avatar/src/lib/WebUtilAvatar.tsx
+++ libs/web/util/avatar/src/lib/WebUtilAvatar.tsx
@@ -1,38 +1,19 @@
 import Image from 'next/image';
+import {
+  WebUtilImageLoaderProps,
+  WebUtilImageLoader,
+} from 'libs/web/util/imageLoader/src';
 
 import styles from './WebUtilAvatar.module.css';
 
 /* eslint-disable-next-line */
-export interface WebUtilAvatarProps {
-  src: string;
-  width: number;
-  height?: number;
-  quality?: string;
-  alt?: string;
-}
-
-interface IimageLoader {
-  src: string | undefined;
-  width: number | undefined;
-  quality?: number | undefined;
-}
-
-const imageLoader = (props: IimageLoader) => {
-  const { src, width, quality } = props;
-  return `${src}?w=${width}&q=${quality || 75}`;
-};
+export interface WebUtilAvatarProps extends WebUtilImageLoaderProps {}
 
 export function WebUtilAvatar(props: WebUtilAvatarProps) {
-  const { src, width, height, quality, alt } = props;
   return (
-    <Image
-      loader={imageLoader}
-      src={src}
-      quality={quality}
-      alt={alt || 'user'}
-      width={width}
-      height={height || width}
-    />
+    <div className={`h-[${props.width}px]`}>
+      <WebUtilImageLoader {...props} />
+    </div>
   );
 }
 
