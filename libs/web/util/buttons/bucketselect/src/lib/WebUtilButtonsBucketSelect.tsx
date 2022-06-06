97d0e80ecc99d4c40732312654f5d1478133d397
erias04 <github.eliascerne@icloud.com>
2022-06-07 00:26:11 +0200

    feat: styled bucket select button

--- libs/web/util/buttons/bucketselect/src/lib/WebUtilButtonsBucketSelect.tsx
+++ libs/web/util/buttons/bucketselect/src/lib/WebUtilButtonsBucketSelect.tsx
@@ -1,16 +1,21 @@
-import styles from './WebUtilButtonsBucketselect.module.css';
+import { AngleDown } from '@nuldrive/web/util/icons';
+
+import styles from './WebUtilButtonsBucketSelect.module.css';
 
 /* eslint-disable-next-line */
-export interface WebUtilButtonsBucketselectProps {}
+export interface WebUtilButtonsBucketSelectProps {}
 
-export function WebUtilButtonsBucketselect(
-  props: WebUtilButtonsBucketselectProps
+export function WebUtilButtonsBucketSelect(
+  props: WebUtilButtonsBucketSelectProps
 ) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebUtilButtonsBucketselect!</h1>
-    </div>
+    <button className="">
+      <div className="flex gap-[2px] rounded-[4px] pl-[7px] pr-[2px] py-[4px] transition-all duration-200 hover:bg-[#EDEDED]">
+        <p className="text-[11px]">school</p>
+        <AngleDown width={21} fill="#6B6B6B" />
+      </div>
+    </button>
   );
 }
 
-export default WebUtilButtonsBucketselect;
+export default WebUtilButtonsBucketSelect;
