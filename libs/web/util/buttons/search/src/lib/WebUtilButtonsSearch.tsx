9ccf54f9063d8e6fa14fb5e23608ff4b7974a339
erias04 <github.eliascerne@icloud.com>
2022-06-07 22:20:42 +0200

    feat: styled search button

--- libs/web/util/buttons/search/src/lib/WebUtilButtonsSearch.tsx
+++ libs/web/util/buttons/search/src/lib/WebUtilButtonsSearch.tsx
@@ -9,23 +9,23 @@ export function WebUtilButtonsSearch(props: WebUtilButtonsSearchProps) {
   return (
     <button
       // onClick={toggleModals}
-      className="bg-primaryColor border-searchBarBorder border-2 pl-4 py-1 pr-2 rounded-xl cursor-pointer w-full"
+      className="bg-[#F2F2F2] border-[#E8E8E8] border-2 pl-4 py-auto pr-2 rounded-xl cursor-pointer w-full h-full"
     >
-      <div className="flex justify-between text-black">
+      <div className="flex justify-between text-[#8494AA]">
         <div className="flex items-center gap-2">
-          <span className="w-5">
+          <span className="w-[16px] fill-[#8494AA]">
             <Search />
           </span>
-          <p className="font-medium text-lg">Search</p>
+          <p className="font-medium text-[14px]">Search</p>
         </div>
         <div className="flex items-center gap-1">
-          <kbd className="self-center rounded-md border-searchBarBorder border-2 px-1 py-0.5">
-            <span className="text-sm">
-              <Command width="1em" height="1em" />
+          <kbd className="self-center rounded-md border-[#E8E8E8] border-2 px-1 py-0.5">
+            <span className="w-[10px] fill-[#8494AA]">
+              <Command width={10} />
             </span>
           </kbd>
-          <kbd className="text-center rounded-md border-searchBarBorder border-2 px-1.5 h-[22px]">
-            <p className="-translate-y-[3px]">K</p>
+          <kbd className="text-center rounded-md border-[#E8E8E8] border-2 w-[17px] h-[18px]">
+            <p className="-translate-y-[2px] text-[12px]">K</p>
           </kbd>
         </div>
       </div>
