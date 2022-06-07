0a0580f127aa86223da3b4ab368795fc887cca66
erias04 <github.eliascerne@icloud.com>
2022-06-07 18:55:48 +0200

    feat: added List to nabigation

--- libs/web/layouts/base/navigation/src/lib/WebLayoutsBaseNavigation.tsx
+++ libs/web/layouts/base/navigation/src/lib/WebLayoutsBaseNavigation.tsx
@@ -1,5 +1,6 @@
 import { Bolt, Folder, User, Times } from '@nuldrive/web/util/icons';
 import { WebUtilButtonsSearch } from '@nuldrive/web/util/buttons/search';
+import { ListUi } from '@nuldrive/web/layouts/base/navigation';
 import Link from 'next/link';
 
 import styles from './WebLayoutsBaseNavigation.module.css';
@@ -14,7 +15,7 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
   return (
     <div className="fixed bottom-0 left-0 w-full bg-primaryColor pt-8 px-6 pb-5 drop-shadow-3xl rounded-2xl border-4 border-gray-700 z-50 md:relative md:w-[80px] md:pt-0 md:px-0 md:pb-0 md:bg-[#F9F9F9] md:h-full md:border-0 md:border-r-border md:border-borderColor md:rounded-none md:drop-shadow-none">
       <div className="grid gap-6 md:flex md:gap-0 h-full w-full md:py-[10px] md:justify-center">
-        <ul className="grid gap-8 grid-cols-3 text-black md:flex md:flex-col md:gap-[25px] md:fill-[#6B6B6B] md:text-[#6B6B6B]">
+        {/* <ul className="grid gap-8 grid-cols-3 text-black md:flex md:flex-col md:gap-[25px] md:fill-[#6B6B6B] md:text-[#6B6B6B]">
           <li className="cursor-pointer">
             <Link href="/link">
               <div className="flex flex-col items-center text-sm font-medium">
@@ -65,7 +66,8 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
               </div>
             </Link>
           </li>
-        </ul>
+        </ul> */}
+        <ListUi />
         <div className="flex justify-between items-center md:hidden">
           <div className="w-[80%] ml-3">
             <WebUtilButtonsSearch />
