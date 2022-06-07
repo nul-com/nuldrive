6a6a7e65e313c91cf2494a286823fdec194fae80
erias04 <github.eliascerne@icloud.com>
2022-06-07 10:13:40 +0200

    create: new lib panel1 in web/layouts/base

---
+++ libs/web/layouts/base/panel1/src/lib/WebLayoutsBasePanel1.tsx
@@ -0,0 +1,14 @@
+import styles from './WebLayoutsBasePanel1.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsBasePanel1Props {}
+
+export function WebLayoutsBasePanel1(props: WebLayoutsBasePanel1Props) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsBasePanel1!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsBasePanel1;
