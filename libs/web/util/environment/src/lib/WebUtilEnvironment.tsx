25fd81080061220b879a5d68ac613e64e925fb96
erias04 <github.eliascerne@icloud.com>
2022-06-07 02:32:14 +0200

    feat: styled environment lib

--- libs/web/util/environment/src/lib/WebUtilEnvironment.tsx
+++ libs/web/util/environment/src/lib/WebUtilEnvironment.tsx
@@ -1,3 +1,5 @@
+import { AngleDown, Eye } from '@nuldrive/web/util/icons';
+
 import styles from './WebUtilEnvironment.module.css';
 
 /* eslint-disable-next-line */
@@ -5,8 +7,14 @@ export interface WebUtilEnvironmentProps {}
 
 export function WebUtilEnvironment(props: WebUtilEnvironmentProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebUtilEnvironment!</h1>
+    <div className="w-[227px] flex-none border-l-border border-borderColor">
+      <div className="flex h-full justify-between items-center gap-[23px] pl-[21px] pr-[14px]">
+        <div className="flex gap-[34px] cursor-pointer">
+          <p className="text-[12px] text-[#6B6B6B]">No Environment</p>
+          <AngleDown width={21} fill="#6B6B6B" />
+        </div>
+        <Eye width={18} fill="#6B6B6B" className="cursor-pointer" />
+      </div>
     </div>
   );
 }
