c092804f17d4ee58ab8d3763e053764bb7c164d3
erias04 <github.eliascerne@icloud.com>
2022-06-07 22:19:07 +0200

    feat: added styling to navigation

--- libs/web/layouts/base/navigation/src/lib/WebLayoutsBaseNavigation.tsx
+++ libs/web/layouts/base/navigation/src/lib/WebLayoutsBaseNavigation.tsx
@@ -13,7 +13,7 @@ export interface WebLayoutsBaseNavigationProps {
 export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
   const { setNavMenuOpen } = props;
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-primaryColor pt-8 px-6 pb-5 drop-shadow-3xl rounded-2xl border-4 border-gray-700 z-100 md:relative md:w-[80px] md:pt-0 md:px-0 md:pb-0 md:bg-[#F9F9F9] md:h-full md:border-0 md:border-r-border md:border-borderColor md:rounded-none md:drop-shadow-none">
+    <div className="fixed bottom-0 left-0 w-full bg-primaryColor pt-8 px-6 pb-5 drop-shadow-3xl rounded-2xl border-4 border-gray-700 z-100 md:static md:w-[80px] md:pt-0 md:px-0 md:pb-0 md:bg-[#F9F9F9] md:h-full md:border-0 md:border-r-border md:border-borderColor md:rounded-none md:drop-shadow-none">
       <div className="grid gap-6 md:flex md:gap-0 md:h-full md:w-full md:py-[10px] md:justify-center overflow-scroll">
         <ListUi />
         <div className="flex justify-between items-center md:hidden">
