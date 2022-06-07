58666229127f1694a15969740830ae5204aee46b
erias04 <github.eliascerne@icloud.com>
2022-06-07 08:16:14 +0200

    feat: changed menu height

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       spacing: {
         header: '47px',
         footer: '24px',
-        menu: '40px',
+        menu: '35px',
       },
       width: {
         7.5: '1.8rem',
