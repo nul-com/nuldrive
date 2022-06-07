c53e0044895083851b94bbc32f2fd3e940065dfe
erias04 <github.eliascerne@icloud.com>
2022-06-07 12:38:20 +0200

    feat: changed content for navigation

--- libs/web/layouts/base/navigation/src/lib/WebLayoutsBaseNavigation.tsx
+++ libs/web/layouts/base/navigation/src/lib/WebLayoutsBaseNavigation.tsx
@@ -13,7 +13,7 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
   const { setNavMenuOpen } = props;
   return (
     <div className="fixed bottom-0 left-0 w-full bg-primaryColor pt-8 px-6 pb-5 drop-shadow-3xl rounded-2xl border-4 border-gray-700 z-50 md:relative md:w-[80px] md:pt-0 md:px-0 md:pb-0 md:bg-[#F9F9F9] md:h-full md:border-0 md:border-r-border md:border-borderColor md:rounded-none md:drop-shadow-none">
-      <div className="grid gap-6 md:flex md:gap-0 bg-cyan-300 h-full w-full md:py-[10px] md:justify-center">
+      <div className="grid gap-6 md:flex md:gap-0 h-full w-full md:py-[10px] md:justify-center">
         <ul className="grid gap-8 grid-cols-3 text-black md:flex md:flex-col md:gap-[25px] md:fill-[#6B6B6B] md:text-[#6B6B6B]">
           <li className="cursor-pointer">
             <Link href="/link">
@@ -21,7 +21,7 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
                 <span className="w-5 md:w-[17px]">
                   <Bolt />
                 </span>
-                <p className="md:text-[10px]">About</p>
+                <p className="md:text-[10px]">Quick</p>
               </div>
             </Link>
           </li>
@@ -31,7 +31,7 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
                 <span className="w-5 md:w-[17px]">
                   <Folder />
                 </span>
-                <p className="md:text-[10px]">FAQ</p>
+                <p className="md:text-[10px]">Collections</p>
               </div>
             </Link>
           </li>
@@ -41,7 +41,27 @@ export function WebLayoutsBaseNavigation(props: WebLayoutsBaseNavigationProps) {
                 <span className="w-5 md:w-[17px]">
                   <User />
                 </span>
-                <p className="md:text-[10px]">User</p>
+                <p className="md:text-[10px]">Photos</p>
+              </div>
+            </Link>
+          </li>
+          <li className="cursor-pointer">
+            <Link href="/link">
+              <div className="flex flex-col items-center text-sm font-medium">
+                <span className="w-5 md:w-[17px]">
+                  <Bolt />
+                </span>
+                <p className="md:text-[10px]">Share</p>
+              </div>
+            </Link>
+          </li>
+          <li className="cursor-pointer">
+            <Link href="/link">
+              <div className="flex flex-col items-center text-sm font-medium">
+                <span className="w-5 md:w-[17px]">
+                  <Bolt />
+                </span>
+                <p className="md:text-[10px]">About</p>
               </div>
             </Link>
           </li>
