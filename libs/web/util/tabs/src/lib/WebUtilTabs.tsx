ab945110d4fc99cf2cefda0e654a279e2f7666ad
erias04 <github.eliascerne@icloud.com>
2022-06-07 08:18:17 +0200

    feat: styled tabs in menu

--- libs/web/util/tabs/src/lib/WebUtilTabs.tsx
+++ libs/web/util/tabs/src/lib/WebUtilTabs.tsx
@@ -11,51 +11,51 @@ export function WebUtilTabs(props: WebUtilTabsProps) {
       <div className="overflow-scroll">
         <div className="flex justify-between h-full">
           <div className="flex items-center">
-          <button className="flex h-[41px] bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
+          <button className="flex h-menu bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">school</p>
             </button>
-            <button className="flex h-[41px] sticky left-0  w-max bg-white items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] border-t-[#ED7547] border">
+            <button className="flex h-menu sticky left-0  w-max bg-white items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] border-t-[#ED7547] border">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">work</p>
             </button>
-            <button className="flex h-[41px] bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
+            <button className="flex h-menu bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">eriasglobe.min.js</p>
             </button>
-            <button className="flex h-[41px] bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
+            {/* <button className="flex h-menu bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">Quick Access</p>
             </button>
-            <button className="flex h-[41px] bg-white items-center w-max gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
+            <button className="flex h-menu bg-white items-center w-max gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">eriasvideo.min.js</p>
             </button>
-            <button className="flex h-[41px] bg-white  items-center w-max gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
+            <button className="flex h-menu bg-white  items-center w-max gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">eriasglider.min.js</p>
             </button>
-            <button className="flex h-[41px] bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
+            <button className="flex h-menu bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">nuldrive.min.js</p>
             </button>
-            <button className="flex h-[41px] bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
+            <button className="flex h-menu bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">work related</p>
             </button>
-            <button className="flex h-[41px] bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
+            <button className="flex h-menu bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">boom</p>
             </button>
-            <button className="flex h-[41px] bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
+            <button className="flex h-menu bg-white w-max items-center gap-[9px] px-[21px] border-r-[1px] border-r-borderColor border-b-[1px] border-white border-t-[2px] ">
               <File fill="#7AC780" width={18} />
               <p className="text-[11px] ">katusch</p>
-            </button>
+            </button> */}
           </div>
 
           <div className="flex gap-[11px] items-centers sticky right-0 z-50 bg-white px-[12px] border-l-border border-borderColor">
-            <Plus fill="#6B6B6B" width={20} />
-            <EllipsisH fill="#6B6B6B" width={20} />
+            <Plus fill="#6B6B6B" width={20} className="cursor-pointer" />
+            <EllipsisH fill="#6B6B6B" width={20} className="cursor-pointer" />
           </div>
         </div>
       </div>
