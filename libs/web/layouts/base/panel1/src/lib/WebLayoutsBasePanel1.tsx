61f863eff13e01d8e488b0366216de43ddb1a43f
erias04 <github.eliascerne@icloud.com>
2022-06-07 22:19:19 +0200

    feat: added search button

--- libs/web/layouts/base/panel1/src/lib/WebLayoutsBasePanel1.tsx
+++ libs/web/layouts/base/panel1/src/lib/WebLayoutsBasePanel1.tsx
@@ -1,4 +1,5 @@
 import { WebLayoutsBaseNavigation } from '@nuldrive/web/layouts/base/navigation';
+import { WebUtilButtonsSearch } from '@nuldrive/web/util/buttons/search';
 
 import styles from './WebLayoutsBasePanel1.module.css';
 
@@ -8,7 +9,12 @@ export interface WebLayoutsBasePanel1Props {}
 export function WebLayoutsBasePanel1(props: WebLayoutsBasePanel1Props) {
   return (
     <div className="hidden md:flex md:h-full">
-      <WebLayoutsBaseNavigation /> <div className="">Hello world</div>
+      <div className="">
+        <WebLayoutsBaseNavigation />
+      </div>
+      <div className="h-[34px] w-full px-3 mt-[13px]">
+        <WebUtilButtonsSearch />
+      </div>
     </div>
   );
 }
