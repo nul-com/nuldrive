bec075451b20f0c32f1976c8420ed413e178d43b
erias04 <github.eliascerne@icloud.com>
2022-06-08 08:18:51 +0200

    feat: added functionality which will be deleted soon

--- libs/web/layouts/base/panel1/src/lib/WebLayoutsBasePanel1.tsx
+++ libs/web/layouts/base/panel1/src/lib/WebLayoutsBasePanel1.tsx
@@ -1,14 +1,56 @@
 import { WebLayoutsBaseNavigation } from '@nuldrive/web/layouts/base/navigation';
 import { WebUtilButtonsSearch } from '@nuldrive/web/util/buttons/search';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from './WebLayoutsBasePanel1.module.css';
 
 /* eslint-disable-next-line */
-export interface WebLayoutsBasePanel1Props {}
+export interface WebLayoutsBasePanel1Props {
+  size: any;
+}
 
 export function WebLayoutsBasePanel1(props: WebLayoutsBasePanel1Props) {
+  const { size } = props;
+  const [searchCollapsed, setSearchCollapsed] = useState<boolean>(
+    size < 250 ? true : false
+  );
+  let divRef = useRef<any>();
+
+  const useContainerDimensions = (myRef: any) => {
+    const getDimensions = () => ({
+      width: myRef?.current?.clientWidth,
+      height: myRef?.current?.clientHeight,
+    });
+
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+      const handleResize = () => {
+        setDimensions(getDimensions());
+      };
+
+      if (myRef.current) {
+        setDimensions(getDimensions());
+      }
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, [myRef]);
+
+    return dimensions;
+  };
+  const { width, height } = useContainerDimensions(divRef);
+  console.log(width);
+
   return (
-    <div className="hidden md:flex md:h-full">
+    <div
+      className="hidden md:flex md:h-full"
+      onClick={() => console.log(size)}
+      ref={divRef}
+    >
       <div className="">
         <WebLayoutsBaseNavigation />
       </div>
