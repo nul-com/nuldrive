1803b7206031920ae652225fc53b0667ae35fc75
erias04 <github.eliascerne@icloud.com>
2022-06-11 21:34:24 +0200

    feat: added layout to index page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,11 @@
+import type { ReactElement } from 'react';
+import { WebLayoutsBaseUi } from '@nuldrive/web/layouts/base/ui';
+
 import styles from './index.module.css';
 
-export function Index() {
+export interface IndexProps {}
+
+export function Index(props: IndexProps) {
   /*
    * Replace the elements below with your own.
    *
@@ -8,5 +13,9 @@ export function Index() {
    */
   return <h1 className="text-xl font-bold">Hello World</h1>;
 }
+
+Index.getLayout = function getLayout(page: ReactElement) {
+  return <WebLayoutsBaseUi>{page}</WebLayoutsBaseUi>;
+};
 
 export default Index;
