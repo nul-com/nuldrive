077f2f6278f9b9cead7a14d7b35a26a5a321fa80
erias04 <github.eliascerne@icloud.com>
2022-06-12 19:20:07 +0200

    create: new lib FeaturesHome in landing/sections

---
+++ libs/web/layouts/landing/sections/features-home/src/lib/WebLayoutsLandingSectionsFeaturesHome.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsFeaturesHome.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsFeaturesHomeProps {}
+
+export function WebLayoutsLandingSectionsFeaturesHome(
+  props: WebLayoutsLandingSectionsFeaturesHomeProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsFeaturesHome!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsFeaturesHome;
