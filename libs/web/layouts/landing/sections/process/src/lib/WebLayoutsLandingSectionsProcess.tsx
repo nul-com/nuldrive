6d3488c9e6ec3389f2407649cfb6535b1607033d
erias04 <github.eliascerne@icloud.com>
2022-06-12 19:17:09 +0200

    create: new lib process in landing/sections

---
+++ libs/web/layouts/landing/sections/process/src/lib/WebLayoutsLandingSectionsProcess.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsProcess.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsProcessProps {}
+
+export function WebLayoutsLandingSectionsProcess(
+  props: WebLayoutsLandingSectionsProcessProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsProcess!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsProcess;
