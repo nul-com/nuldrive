efc3ef2c5a5a8bba26f8d271a76c84a39319ba83
erias04 <github.eliascerne@icloud.com>
2022-06-12 19:08:46 +0200

    feat: changed background color for modal

--- libs/web/layouts/landing/hero/src/lib/modal/modal.tsx
+++ libs/web/layouts/landing/hero/src/lib/modal/modal.tsx
@@ -39,7 +39,7 @@ export function Modal(props: ModalProps) {
     <>
       {/* Modal backdrop */}
       <Transition
-        className="fixed inset-0 z-50 bg-white bg-opacity-75 transition-opacity backdrop-blur-sm"
+        className="fixed inset-0 z-50 bg-gray-800 bg-opacity-75 transition-opacity backdrop-blur-sm"
         show={show}
         enter="transition ease-out duration-200"
         enterStart="opacity-0"
