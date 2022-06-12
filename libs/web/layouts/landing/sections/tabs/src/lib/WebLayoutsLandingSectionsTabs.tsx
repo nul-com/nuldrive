bec1b03136bc87139b4a0d74dae56a5fcb028715
erias04 <github.eliascerne@icloud.com>
2022-06-12 19:24:39 +0200

    create: new lib Tabs in landing/sections

---
+++ libs/web/layouts/landing/sections/tabs/src/lib/WebLayoutsLandingSectionsTabs.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsTabs.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsTabsProps {}
+
+export function WebLayoutsLandingSectionsTabs(
+  props: WebLayoutsLandingSectionsTabsProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsTabs!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsTabs;
