ce67e13c8387c1c49768e60b5809afb415175a20
erias04 <github.eliascerne@icloud.com>
2022-06-12 20:34:48 +0200

    create: new lib newsletter in landing/sections

---
+++ libs/web/layouts/landing/sections/newsletter/src/lib/WebLayoutsLandingSectionsNewsletter.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsNewsletter.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsNewsletterProps {}
+
+export function WebLayoutsLandingSectionsNewsletter(
+  props: WebLayoutsLandingSectionsNewsletterProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsNewsletter!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsNewsletter;
