0c00fdd801b9e1ef1948ad11133e1c4cd9c1bb79
erias04 <github.eliascerne@icloud.com>
2022-06-12 19:38:35 +0200

    create: new lib news to landing/sections

---
+++ libs/web/layouts/landing/sections/news/src/lib/WebLayoutsLandingSectionsNews.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsNews.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsNewsProps {}
+
+export function WebLayoutsLandingSectionsNews(
+  props: WebLayoutsLandingSectionsNewsProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsNews!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsNews;
