c32c577c43878816e55cc417ea7697a46dde2683
erias04 <github.eliascerne@icloud.com>
2022-06-12 23:01:52 +0200

    feat: changed styling for Section Tab

--- libs/web/layouts/landing/sections/tabs/src/lib/WebLayoutsLandingSectionsTabs.tsx
+++ libs/web/layouts/landing/sections/tabs/src/lib/WebLayoutsLandingSectionsTabs.tsx
@@ -28,7 +28,7 @@ export function WebLayoutsLandingSectionsTabs(
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20 border-t border-gray-800">
+        <div className="py-12 md:py-20 mt-24 border-t border-gray-800">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12" data-aos-id-tabs>
             <h2
