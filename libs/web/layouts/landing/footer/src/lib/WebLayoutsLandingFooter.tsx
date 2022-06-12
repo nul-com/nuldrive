6d8b92f6edf63120858838be4eb84aecebfca821
erias04 <github.eliascerne@icloud.com>
2022-06-12 20:41:20 +0200

    create: new lib Footer in layouts/landing

---
+++ libs/web/layouts/landing/footer/src/lib/WebLayoutsLandingFooter.tsx
@@ -0,0 +1,14 @@
+import styles from './WebLayoutsLandingFooter.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingFooterProps {}
+
+export function WebLayoutsLandingFooter(props: WebLayoutsLandingFooterProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingFooter!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingFooter;
