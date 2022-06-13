163a3829727afe4a695e9e9159d0ae4cd545f1d0
erias04 <github.eliascerne@icloud.com>
2022-06-13 09:30:12 +0200

    feat: create: new lib statsCircle in landing/sections

---
+++ libs/web/layouts/landing/sections/stats-circle/src/lib/WebLayoutsLandingSectionsStatsCircle.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsStatsCircle.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsStatsCircleProps {}
+
+export function WebLayoutsLandingSectionsStatsCircle(
+  props: WebLayoutsLandingSectionsStatsCircleProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsStatsCircle!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsStatsCircle;
