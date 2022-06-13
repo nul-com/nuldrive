b8c393d663d7e19c2b8c8a99a688219d9d9af4b7
erias04 <github.eliascerne@icloud.com>
2022-06-13 22:31:57 +0200

    feat: added animations to featuresExplore

--- libs/web/layouts/landing/sections/features-explore/src/lib/WebLayoutsLandingSectionsFeaturesExplore.tsx
+++ libs/web/layouts/landing/sections/features-explore/src/lib/WebLayoutsLandingSectionsFeaturesExplore.tsx
@@ -39,8 +39,10 @@ export function WebLayoutsLandingSectionsFeaturesExplore(
         <div className="pt-12 md:pt-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h2 mb-4">How Simple works</h1>
-            <p className="text-xl text-gray-400">
+            <h1 data-aos="fade-up" className="h2 mb-4">
+              How Simple works
+            </h1>
+            <p data-aos="fade-down" className="text-xl text-gray-400">
               Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur excepteur sint occaecat
               cupidatat.
