338321c20e66af67ad0a84aa53179d8d8ac6d6f9
erias04 <github.eliascerne@icloud.com>
2022-06-13 23:22:03 +0200

    create: new lib support in landing/sections

---
+++ libs/web/layouts/landing/sections/support/src/lib/WebLayoutsLandingSectionsSupport.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsSupport.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsSupportProps {}
+
+export function WebLayoutsLandingSectionsSupport(
+  props: WebLayoutsLandingSectionsSupportProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsSupport!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsSupport;
