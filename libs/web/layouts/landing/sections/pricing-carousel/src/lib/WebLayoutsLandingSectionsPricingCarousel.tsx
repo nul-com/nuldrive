eddea2878e046eae944616511dc9a623e9933db8
erias04 <github.eliascerne@icloud.com>
2022-06-13 21:59:20 +0200

    feat: added animation to pricingCaroisel

--- libs/web/layouts/landing/sections/pricing-carousel/src/lib/WebLayoutsLandingSectionsPricingCarousel.tsx
+++ libs/web/layouts/landing/sections/pricing-carousel/src/lib/WebLayoutsLandingSectionsPricingCarousel.tsx
@@ -205,10 +205,14 @@ export function WebLayoutsLandingSectionsPricingCarousel(
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h2 className="h2 mb-4">
+            <h2 className="h2 mb-4" data-aos="fade-up">
               Trusted by over 20,000 companies all over the world
             </h2>
-            <p className="text-xl text-gray-400" data-aos="zoom-y-out">
+            <p
+              className="text-xl text-gray-400"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
               Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar
               mattis blandit libero cursus mattis.
             </p>
@@ -217,7 +221,11 @@ export function WebLayoutsLandingSectionsPricingCarousel(
           {/* Items */}
           <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-4 md:grid-cols-5">
             {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
+            <div
+              data-aos="zoom-out"
+              data-aos-delay="200"
+              className="flex items-center justify-center py-2 col-span-2 md:col-auto"
+            >
               <svg
                 className="max-w-full fill-current text-gray-400"
                 width="124"
@@ -230,7 +238,11 @@ export function WebLayoutsLandingSectionsPricingCarousel(
             </div>
 
             {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
+            <div
+              data-aos="zoom-out"
+              data-aos-delay="200"
+              className="flex items-center justify-center py-2 col-span-2 md:col-auto"
+            >
               <svg
                 className="max-w-full fill-current text-gray-400"
                 width="83"
@@ -243,7 +255,11 @@ export function WebLayoutsLandingSectionsPricingCarousel(
             </div>
 
             {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
+            <div
+              data-aos="zoom-out"
+              data-aos-delay="200"
+              className="flex items-center justify-center py-2 col-span-2 md:col-auto"
+            >
               <svg
                 className="max-w-full fill-current text-gray-400"
                 width="125"
@@ -256,7 +272,11 @@ export function WebLayoutsLandingSectionsPricingCarousel(
             </div>
 
             {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
+            <div
+              data-aos="zoom-out"
+              data-aos-delay="200"
+              className="flex items-center justify-center py-2 col-span-2 md:col-auto"
+            >
               <svg
                 className="max-w-full fill-current text-gray-400"
                 width="113"
@@ -269,7 +289,11 @@ export function WebLayoutsLandingSectionsPricingCarousel(
             </div>
 
             {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto col-start-2 col-end-4">
+            <div
+              data-aos="zoom-out"
+              data-aos-delay="200"
+              className="flex items-center justify-center py-2 col-span-2 md:col-auto col-start-2 col-end-4"
+            >
               <svg
                 className="max-w-full fill-current text-gray-400"
                 width="109"
@@ -314,6 +338,8 @@ export function WebLayoutsLandingSectionsPricingCarousel(
                         <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
                       </svg>
                       <img
+                        data-aos="fade-down"
+                        data-aos-delay="200"
                         className="relative rounded-full"
                         src={item.img}
                         width="96"
@@ -321,13 +347,25 @@ export function WebLayoutsLandingSectionsPricingCarousel(
                         alt={item.alt}
                       />
                     </div>
-                    <blockquote className="text-xl font-medium mb-4 text-gray-300">
+                    <blockquote
+                      data-aos="fade-up"
+                      data-aos-delay="200"
+                      className="text-xl font-medium mb-4 text-gray-300"
+                    >
                       {item.quote}
                     </blockquote>
-                    <cite className="block font-bold text-lg not-italic mb-1 text-gray-400">
+                    <cite
+                      data-aos="fade-up"
+                      data-aos-delay="200"
+                      className="block font-bold text-lg not-italic mb-1 text-gray-400"
+                    >
                       {item.name}
                     </cite>
-                    <div className="text-gray-400">
+                    <div
+                      data-aos="fade-up"
+                      data-aos-delay="200"
+                      className="text-gray-400"
+                    >
                       <span>{item.role}</span>{' '}
                       <span className="text-blue-600 hover:underline">
                         <Link href={item.link}>{item.team}</Link>
