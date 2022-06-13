6d355bc2b23a571c7f68dbbb5760b1067721728a
erias04 <github.eliascerne@icloud.com>
2022-06-13 11:01:45 +0200

    create: new lib aboutStory in landing/sections

---
+++ libs/web/layouts/landing/sections/about-story/src/lib/WebLayoutsLandingSectionsAboutStory.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsAboutStory.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsAboutStoryProps {}
+
+export function WebLayoutsLandingSectionsAboutStory(
+  props: WebLayoutsLandingSectionsAboutStoryProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsAboutStory!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsAboutStory;
