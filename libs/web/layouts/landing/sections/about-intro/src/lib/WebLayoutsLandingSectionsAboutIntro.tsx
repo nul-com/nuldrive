7bb3617bb5fa8d05d6d78ab64b2dd35c2965cef4
erias04 <github.eliascerne@icloud.com>
2022-06-13 11:00:44 +0200

    feat: added content and styling to aboutIntro

--- libs/web/layouts/landing/sections/about-intro/src/lib/WebLayoutsLandingSectionsAboutIntro.tsx
+++ libs/web/layouts/landing/sections/about-intro/src/lib/WebLayoutsLandingSectionsAboutIntro.tsx
@@ -7,9 +7,33 @@ export function WebLayoutsLandingSectionsAboutIntro(
   props: WebLayoutsLandingSectionsAboutIntroProps
 ) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebLayoutsLandingSectionsAboutIntro!</h1>
-    </div>
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <h1 className="h1 mb-4">
+              We enable developers to build amazing things
+            </h1>
+            <p className="text-xl text-gray-400">
+              We have transformed product development, making it faster,
+              simpler... better! That's why in just three years we now help more
+              developers build projects than anyone else.
+            </p>
+          </div>
+
+          {/* <figure className="flex justify-center items-start">
+            <img
+              className="rounded-xl shadow-2xl z-20"
+              src="https://r2.eriascdn.com/about-01.jpg"
+              width="768"
+              height="432"
+              alt="About us"
+            />
+          </figure> */}
+        </div>
+      </div>
+    </section>
   );
 }
 
