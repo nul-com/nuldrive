e9ae9391d0e352db423832d61f499a9963fc56d1
erias04 <github.eliascerne@icloud.com>
2022-06-13 10:55:18 +0200

    feat: added aboutIntro Lib in landing/section

---
+++ libs/web/layouts/landing/sections/about-intro/src/lib/WebLayoutsLandingSectionsAboutIntro.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsAboutIntro.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsAboutIntroProps {}
+
+export function WebLayoutsLandingSectionsAboutIntro(
+  props: WebLayoutsLandingSectionsAboutIntroProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsAboutIntro!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsAboutIntro;
