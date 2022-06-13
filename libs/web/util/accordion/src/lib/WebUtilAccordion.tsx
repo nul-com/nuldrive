992d71e278117184c319db31ea7a0b898c8343b9
erias04 <github.eliascerne@icloud.com>
2022-06-13 21:39:10 +0200

    feat: added accordion functionality

--- libs/web/util/accordion/src/lib/WebUtilAccordion.tsx
+++ libs/web/util/accordion/src/lib/WebUtilAccordion.tsx
@@ -1,13 +1,77 @@
+import { useState, useRef, useEffect, ReactChild } from 'react';
+
 import styles from './WebUtilAccordion.module.css';
 
 /* eslint-disable-next-line */
-export interface WebUtilAccordionProps {}
+export interface WebUtilAccordionProps {
+  children: ReactChild;
+  tag?: string;
+  title: any;
+  active?: any;
+}
 
 export function WebUtilAccordion(props: WebUtilAccordionProps) {
+  const { children, tag = 'li', title, active } = props;
+
+  const [accordionOpen, setAccordionOpen] = useState<any>(false);
+  const accordion = useRef<any>(null);
+  const Component: any = tag;
+
+  useEffect(() => {
+    setAccordionOpen(active);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [accordion]);
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebUtilAccordion!</h1>
-    </div>
+    <Component>
+      <div className="border-t border-gray-700">
+        <button
+          className="flex items-center w-full text-lg font-medium text-left py-5 border-t border-gray-700"
+          onClick={(e) => {
+            e.preventDefault();
+            setAccordionOpen(!accordionOpen);
+          }}
+          aria-expanded={accordionOpen}
+        >
+          <svg
+            className="w-4 h-4 fill-current text-blue-500 shrink-0 mr-8 -ml-12"
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              y="7"
+              width="16"
+              height="2"
+              rx="1"
+              className={`transform origin-center transition duration-200 ease-out ${
+                accordionOpen && 'rotate-180'
+              }`}
+            />
+            <rect
+              y="7"
+              width="16"
+              height="2"
+              rx="1"
+              className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+                accordionOpen && 'rotate-180'
+              }`}
+            />
+          </svg>
+          <span>{title}</span>
+        </button>
+      </div>
+      <div
+        ref={accordion}
+        className="text-gray-400 overflow-hidden transition-all duration-300 ease-in-out"
+        style={
+          accordionOpen
+            ? { maxHeight: accordion.current.scrollHeight, opacity: 1 }
+            : { maxHeight: 0, opacity: 0 }
+        }
+      >
+        <p className="pb-5">{children}</p>
+      </div>
+    </Component>
   );
 }
 
