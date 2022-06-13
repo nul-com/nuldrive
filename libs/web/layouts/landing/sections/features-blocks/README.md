f4837ca87787eddff2c0589eaf01228aad6ac104
erias04 <github.eliascerne@icloud.com>
2022-06-13 09:36:42 +0200

    create: lib featuresBlocks

---
+++ libs/web/layouts/landing/sections/features-blocks/README.md
@@ -0,0 +1,7 @@
+# web-layouts-landing-sections-features-blocks
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test web-layouts-landing-sections-features-blocks` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/web/layouts/landing/sections/features-blocks/src/lib/WebLayoutsLandingSectionsFeaturesBlocks.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsFeaturesBlocks.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsFeaturesBlocksProps {}
+
+export function WebLayoutsLandingSectionsFeaturesBlocks(
+  props: WebLayoutsLandingSectionsFeaturesBlocksProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsFeaturesBlocks!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsFeaturesBlocks;
