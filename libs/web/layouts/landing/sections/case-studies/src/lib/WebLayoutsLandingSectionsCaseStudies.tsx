8dfa4f6140a9ef0b9cdae5ebf7a43b4082919113
erias04 <github.eliascerne@icloud.com>
2022-06-13 09:42:54 +0200

    create: new lib case-studies

---
+++ libs/web/layouts/landing/sections/case-studies/src/lib/WebLayoutsLandingSectionsCaseStudies.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsCaseStudies.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsCaseStudiesProps {}
+
+export function WebLayoutsLandingSectionsCaseStudies(
+  props: WebLayoutsLandingSectionsCaseStudiesProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsCaseStudies!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsCaseStudies;
