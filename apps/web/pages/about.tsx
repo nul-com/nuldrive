bad00434dfa5ea2696f4f13a152b59021b64fced
erias04 <github.eliascerne@icloud.com>
2022-06-13 18:56:52 +0200

    feat: added aboutProcess to about page

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -10,6 +10,7 @@ import { WebLayoutsLandingSectionsTestimonialCarousel } from '@nuldrive/web/layo
 import { WebLayoutsLandingSectionsCareer } from 'libs/web/layouts/landing/sections/career/src';
 import { WebLayoutsLandingSectionsAboutClients } from 'libs/web/layouts/landing/sections/about-clients/src';
 import { WebLayoutsLandingSectionsNewsletter } from '@nuldrive/web/layouts/landing/sections//newsletter';
+import { WebLayoutsLandingSectionsProcess } from '@nuldrive/web/layouts/landing/sections/process';
 
 export interface AboutProps {}
 
@@ -29,6 +30,7 @@ export function About(props: AboutProps) {
       <WebLayoutsLandingSectionsTeam />
       <WebLayoutsLandingSectionsTestimonialCarousel />
       <WebLayoutsLandingSectionsCareer />
+      <WebLayoutsLandingSectionsProcess />
       <WebLayoutsLandingSectionsAboutClients />
       <WebLayoutsLandingSectionsNewsletter />
     </div>
