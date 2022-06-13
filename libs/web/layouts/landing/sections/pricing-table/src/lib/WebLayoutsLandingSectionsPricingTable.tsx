349b20f3ffd54017b6c27c323f2da7ba9347f528
erias04 <github.eliascerne@icloud.com>
2022-06-13 19:53:31 +0200

    feat: changed z-index in pricingTable

--- libs/web/layouts/landing/sections/pricing-table/src/lib/WebLayoutsLandingSectionsPricingTable.tsx
+++ libs/web/layouts/landing/sections/pricing-table/src/lib/WebLayoutsLandingSectionsPricingTable.tsx
@@ -26,7 +26,7 @@ export function WebLayoutsLandingSectionsPricingTable(
   });
 
   return (
-    <section className="relative">
+    <section className="relative z-20">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           {/* Section header */}
