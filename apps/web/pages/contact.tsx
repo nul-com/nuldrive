68099bfd347406af7314bcd5c81cfce3d58f3ce0
erias04 <github.eliascerne@icloud.com>
2022-06-13 23:30:52 +0200

    feat: added illustration and support to contact page

--- apps/web/pages/contact.tsx
+++ apps/web/pages/contact.tsx
@@ -1,10 +1,22 @@
 import { ReactElement } from 'react';
 import { WebLayoutsLandingUi } from '@nuldrive/web/layouts/landing/ui';
+import { WebLayoutsLandingSectionsSupport } from 'libs/web/layouts/landing/sections/support/src';
+import { WebLayoutsLandingIllustration } from '@nuldrive/web-layouts-landing-sections-illustration';
 
 export interface ContactProps {}
 
 export function Contact(props: ContactProps) {
-  return <div className=""></div>;
+  return (
+    <main className="grow">
+      <div
+        className="relative max-w-6xl mx-auto h-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        <WebLayoutsLandingIllustration />
+      </div>
+      <WebLayoutsLandingSectionsSupport />
+    </main>
+  );
 }
 
 Contact.getLayout = function getLayout(page: ReactElement) {
