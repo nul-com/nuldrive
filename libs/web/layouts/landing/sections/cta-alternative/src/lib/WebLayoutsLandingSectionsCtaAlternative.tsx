342e83ca252a792f00d245c2d5dc7b043c2bf3ac
erias04 <github.eliascerne@icloud.com>
2022-06-13 19:02:10 +0200

    feat: added content and styling to CtaAlternate

--- libs/web/layouts/landing/sections/cta-alternative/src/lib/WebLayoutsLandingSectionsCtaAlternative.tsx
+++ libs/web/layouts/landing/sections/cta-alternative/src/lib/WebLayoutsLandingSectionsCtaAlternative.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import styles from './WebLayoutsLandingSectionsCtaAlternative.module.css';
 
 /* eslint-disable-next-line */
@@ -7,9 +9,33 @@ export function WebLayoutsLandingSectionsCtaAlternative(
   props: WebLayoutsLandingSectionsCtaAlternativeProps
 ) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to WebLayoutsLandingSectionsCtaAlternative!</h1>
-    </div>
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pb-12 md:pb-20">
+          {/* CTA box */}
+          <div
+            className="bg-purple-600 rounded-2xl py-10 px-8 md:py-16 md:px-12 shadow-2xl"
+            data-aos="zoom-y-out"
+          >
+            <div className="flex flex-col lg:flex-row justify-between items-center">
+              {/* CTA content */}
+              <div className="mb-6 lg:mr-16 lg:mb-0 text-center lg:text-left lg:w-1/2">
+                <h3 className="h3 text-white">
+                  Sounds like we are building something cool?
+                </h3>
+              </div>
+
+              {/* CTA button */}
+              <Link href="/signup">
+                <div className="btn text-purple-600 bg-purple-100 hover:bg-white cursor-pointer">
+                  Join us today
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
