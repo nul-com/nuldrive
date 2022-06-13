066236481e46842a5b6cd8ba7661e7ba8eec7f99
erias04 <github.eliascerne@icloud.com>
2022-06-13 23:12:58 +0200

    create: new lib awesome-cta in landing/sections

---
+++ libs/web/layouts/landing/sections/awesome-cta/src/lib/WebLayoutsLandingSectionsAwesomeCta.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsAwesomeCta.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsAwesomeCtaProps {}
+
+export function WebLayoutsLandingSectionsAwesomeCta(
+  props: WebLayoutsLandingSectionsAwesomeCtaProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsAwesomeCta!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsAwesomeCta;
