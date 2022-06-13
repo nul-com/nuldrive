81c27c7396c50cc582d560433bd4bcb98830059c
erias04 <github.eliascerne@icloud.com>
2022-06-13 11:08:15 +0200

    create: new lib timeline to landing/section

---
+++ libs/web/layouts/landing/sections/timeline/src/lib/WebLayoutsLandingSectionsTimeline.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsTimeline.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsTimelineProps {}
+
+export function WebLayoutsLandingSectionsTimeline(
+  props: WebLayoutsLandingSectionsTimelineProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsTimeline!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsTimeline;
