4be29d0b13c725dd67245d99c76081cfd09d25e5
erias04 <github.eliascerne@icloud.com>
2022-06-13 18:57:44 +0200

    create: new lib CtaAlternative in landing/sections

---
+++ libs/web/layouts/landing/sections/cta-alternative/src/lib/WebLayoutsLandingSectionsCtaAlternative.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsCtaAlternative.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsCtaAlternativeProps {}
+
+export function WebLayoutsLandingSectionsCtaAlternative(
+  props: WebLayoutsLandingSectionsCtaAlternativeProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsCtaAlternative!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsCtaAlternative;
