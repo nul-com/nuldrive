6c6247d65a50a6093cc965d9dad89899a4e273ca
erias04 <github.eliascerne@icloud.com>
2022-06-13 21:38:44 +0200

    feat: changed import in Faq

--- libs/web/layouts/landing/sections/faq/src/lib/WebLayoutsLandingSectionsFaq.tsx
+++ libs/web/layouts/landing/sections/faq/src/lib/WebLayoutsLandingSectionsFaq.tsx
@@ -1,3 +1,5 @@
+import { WebUtilAccordion } from 'libs/web/util/accordion/src';
+
 import styles from './WebLayoutsLandingSectionsFaq.module.css';
 
 /* eslint-disable-next-line */
@@ -9,7 +11,7 @@ export function WebLayoutsLandingSectionsFaq(
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20 border-t border-gray-200">
+        <div className="py-12 md:py-20 border-t border-gray-700">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-20">
             <h2 className="h2">Questions & Answers</h2>
@@ -17,25 +19,25 @@ export function WebLayoutsLandingSectionsFaq(
 
           {/* Faqs */}
           <ul className="max-w-3xl mx-auto pl-12">
-            <Accordion title="Do I need coding knowledge to use this product?">
+            <WebUtilAccordion title="Do I need coding knowledge to use this product?">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
-            </Accordion>
-            <Accordion title="Do you have student or non-profit discounts?">
+            </WebUtilAccordion>
+            <WebUtilAccordion title="Do you have student or non-profit discounts?">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
-            </Accordion>
-            <Accordion title="Is there a way to become an Affiliate reseller of this product?">
+            </WebUtilAccordion>
+            <WebUtilAccordion title="Is there a way to become an Affiliate reseller of this product?">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
-            </Accordion>
-            <Accordion
+            </WebUtilAccordion>
+            <WebUtilAccordion
               title="What is the difference between the Free and Paid versions?"
               active
             >
@@ -43,15 +45,15 @@ export function WebLayoutsLandingSectionsFaq(
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
-            </Accordion>
-            <Accordion title="How can I change the owner of a workspace?">
+            </WebUtilAccordion>
+            <WebUtilAccordion title="How can I change the owner of a workspace?">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
-            </Accordion>
+            </WebUtilAccordion>
             <span
-              className="block border-t border-gray-200"
+              className="block border-t border-gray-700"
               aria-hidden="true"
             ></span>
           </ul>
