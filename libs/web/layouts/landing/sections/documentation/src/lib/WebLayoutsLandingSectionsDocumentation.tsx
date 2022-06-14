5525cf1d6eb66e9a3bed26f12122d5d6f2c6698f
erias04 <github.eliascerne@icloud.com>
2022-06-14 09:29:54 +0200

    create: new lib documentation in landing/sections

---
+++ libs/web/layouts/landing/sections/documentation/src/lib/WebLayoutsLandingSectionsDocumentation.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsDocumentation.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsDocumentationProps {}
+
+export function WebLayoutsLandingSectionsDocumentation(
+  props: WebLayoutsLandingSectionsDocumentationProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsDocumentation!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsDocumentation;
