4ba20f51310ec0bec7cb58e6070629bedd329684
erias04 <github.eliascerne@icloud.com>
2022-06-14 14:11:35 +0200

    create: new lib tutorialsCta in landing/sections

---
+++ libs/web/layouts/landing/sections/tutorials-cta/src/lib/WebLayoutsLandingSectionsTutorialsCta.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsTutorialsCta.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsTutorialsCtaProps {}
+
+export function WebLayoutsLandingSectionsTutorialsCta(
+  props: WebLayoutsLandingSectionsTutorialsCtaProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsTutorialsCta!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsTutorialsCta;
