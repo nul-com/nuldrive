cc22c21a49270f7df391442c6919827dd6a239bc
erias04 <github.eliascerne@icloud.com>
2022-06-14 18:11:03 +0200

    create: new lib post in landing/sections

---
+++ libs/web/layouts/landing/sections/post/src/lib/WebLayoutsLandingSectionsPost.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsPost.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsPostProps {}
+
+export function WebLayoutsLandingSectionsPost(
+  props: WebLayoutsLandingSectionsPostProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsPost!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsPost;
