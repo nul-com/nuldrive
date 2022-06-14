f9ee3f8c7a2bba2a03249b8ae26a3ce0ff4af116
erias04 <github.eliascerne@icloud.com>
2022-06-14 12:14:44 +0200

    feat: inititialized sticky in Landing Layout

--- libs/web/layouts/landing/ui/src/lib/WebLayoutsLandingUi.tsx
+++ libs/web/layouts/landing/ui/src/lib/WebLayoutsLandingUi.tsx
@@ -2,6 +2,7 @@ import { Fragment, ReactChild, useEffect } from 'react';
 import { WebLayoutsLandingHeader } from '@nuldrive/web/layouts/landing/header';
 import { WebLayoutsLandingFooter } from '@nuldrive/web/layouts/landing/footer';
 import AOS from 'aos';
+import Sticky from 'sticky-js';
 
 import styles from './WebLayoutsLandingUi.module.css';
 
@@ -18,8 +19,12 @@ export function WebLayoutsLandingUi(props: WebLayoutsLandingUiProps) {
       duration: 600,
       easing: 'ease-out-sine',
     });
+    // eslint-disable-next-line no-unused-vars
+    const sticky = new Sticky('[data-sticky]');
   });
+
   const { children } = props;
+
   return (
     <div className="overflow-hidden flex flex-col min-h-screen font-inter antialiased bg-gray-900 text-gray-200 tracking-tight">
       <WebLayoutsLandingHeader />
