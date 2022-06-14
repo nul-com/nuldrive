d6e86694c8b4cda4c9724d24cd53dc940396a9ae
erias04 <github.eliascerne@icloud.com>
2022-06-14 08:46:18 +0200

    create: new lib helpCenter in landing/sections

---
+++ libs/web/layouts/landing/sections/help-center/src/lib/WebLayoutsLandingSectionsHelpCenter.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsHelpCenter.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsHelpCenterProps {}
+
+export function WebLayoutsLandingSectionsHelpCenter(
+  props: WebLayoutsLandingSectionsHelpCenterProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsHelpCenter!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsHelpCenter;
