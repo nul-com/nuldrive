552f555a3de32ddb1f63a6e454f8594f1c0a26e4
erias04 <github.eliascerne@icloud.com>
2022-06-14 12:17:34 +0200

    create: new lib tutorialsList in landing/sections

---
+++ libs/web/layouts/landing/sections/tutorials-list/src/lib/WebLayoutsLandingSectionsTutorialsList.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsTutorialsList.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsTutorialsListProps {}
+
+export function WebLayoutsLandingSectionsTutorialsList(
+  props: WebLayoutsLandingSectionsTutorialsListProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsTutorialsList!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsTutorialsList;
