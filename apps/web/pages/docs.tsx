4d8bd3eeeb435cee5c36274f578c74f1f3cb15e7
erias04 <github.eliascerne@icloud.com>
2022-06-14 12:13:33 +0200

    feat: added documentationt to docs page

--- apps/web/pages/docs.tsx
+++ apps/web/pages/docs.tsx
@@ -1,10 +1,15 @@
 import { ReactElement } from 'react';
 import { WebLayoutsLandingUi } from '@nuldrive/web/layouts/landing/ui';
+import { WebLayoutsLandingSectionsDocumentation } from '@nuldrive/web/layouts/landing/sections/documentation';
 
 export interface DocsProps {}
 
 export function Docs(props: DocsProps) {
-  return <main className="grow"></main>;
+  return (
+    <main className="grow">
+      <WebLayoutsLandingSectionsDocumentation />
+    </main>
+  );
 }
 
 Docs.getLayout = function getLayout(page: ReactElement) {
