d53b34664dbba34664bfc454f9dea088db7745c7
erias04 <github.eliascerne@icloud.com>
2022-06-14 08:49:17 +0200

    feat: added illustration and helpCenter to help page

--- apps/web/pages/help.tsx
+++ apps/web/pages/help.tsx
@@ -1,10 +1,22 @@
 import { ReactElement } from 'react';
 import { WebLayoutsLandingUi } from '@nuldrive/web/layouts/landing/ui';
+import { WebLayoutsLandingIllustration } from '@nuldrive/web-layouts-landing-sections-illustration';
+import { WebLayoutsLandingSectionsHelpCenter } from 'libs/web/layouts/landing/sections/help-center/src';
 
 export interface HelpProps {}
 
 export function Help(props: HelpProps) {
-  return <main className="grow"></main>;
+  return (
+    <main className="grow">
+      <div
+        className="relative max-w-6xl mx-auto h-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        <WebLayoutsLandingIllustration />
+      </div>
+      <WebLayoutsLandingSectionsHelpCenter />
+    </main>
+  );
 }
 
 Help.getLayout = function getLayout(page: ReactElement) {
