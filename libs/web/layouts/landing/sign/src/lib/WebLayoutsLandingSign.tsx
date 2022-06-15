b2fd565c77025d99b4ad35a71b4327194c64e455
erias04 <github.eliascerne@icloud.com>
2022-06-15 18:49:38 +0200

    create: new lib sign in layouts/landing

---
+++ libs/web/layouts/landing/sign/src/lib/WebLayoutsLandingSign.tsx
@@ -0,0 +1,14 @@
+import styles from './WebLayoutsLandingSign.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSignProps {}
+
+export function WebLayoutsLandingSign(props: WebLayoutsLandingSignProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSign!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSign;
