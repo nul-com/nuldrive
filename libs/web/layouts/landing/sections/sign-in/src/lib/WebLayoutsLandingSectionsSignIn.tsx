b8b872f68056e69fa77362076fb7943ef8eadfbc
erias04 <github.eliascerne@icloud.com>
2022-06-16 20:08:01 +0200

    create: new lib signIn in landing/sections

---
+++ libs/web/layouts/landing/sections/sign-in/src/lib/WebLayoutsLandingSectionsSignIn.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsSignIn.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsSignInProps {}
+
+export function WebLayoutsLandingSectionsSignIn(
+  props: WebLayoutsLandingSectionsSignInProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsSignIn!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsSignIn;
