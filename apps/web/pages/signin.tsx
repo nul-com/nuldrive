388a9ac3c3ecb768a755a54b00f6910cb1805055
erias04 <github.eliascerne@icloud.com>
2022-06-16 17:25:53 +0200

    feat: added signin functionality

--- apps/web/pages/signin.tsx
+++ apps/web/pages/signin.tsx
@@ -1,19 +1,23 @@
-import { ReactElement, useEffect, useRef } from 'react';
+import { ReactElement, useEffect, useRef, useState } from 'react';
 import { WebLayoutsLandingSign } from '@nuldrive/web/layouts/landing/sign';
-import { WebLayoutsLandingIllustration } from '@nuldrive/web-layouts-landing-sections-illustration';
 import { WebLayoutsLandingSectionsSignIn } from '@nuldrive/web/layouts/landing/sections/sign-in';
 import WebUtilSpace from '@nuldrive/space';
+import { WebLayoutsLandingSectionsSign } from '@nuldrive/web/layouts/landing/sections/sign';
 
 export interface SignInProps {}
 export function SignIn(props: SignInProps) {
   const canvasRef = useRef<HTMLCanvasElement>();
+  const [space, setSpace] = useState<any>();
+
   useEffect(() => {
     const space = new WebUtilSpace({ canvas: canvasRef.current });
     space.start();
+    setSpace(space);
   }, []);
+
   return (
     <main className="grow">
-      <WebLayoutsLandingSectionsSignIn />
+      <WebLayoutsLandingSectionsSign space={space} />
       <canvas className="fixed top-0 w-full h-full" ref={canvasRef}></canvas>
     </main>
   );
