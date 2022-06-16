af29f71bd278341409b53e94a728cbc25a0dfb0d
erias04 <github.eliascerne@icloud.com>
2022-06-16 17:50:46 +0200

    feat: added loadingBar on route changed to app.tsx

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,7 +1,10 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import type { ReactElement, ReactNode } from 'react';
+import { useEffect } from 'react';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
 
 import './styles.css';
 import '../public/styles/style.scss';
@@ -15,7 +18,29 @@ type AppPropsWithLayout = AppProps & {
 };
 
 function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter();
+
   const getLayout = Component.getLayout ?? ((page) => page);
+
+  useEffect(() => {
+    const handleStart = (url: any) => {
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return (
     <>
       <Head>
