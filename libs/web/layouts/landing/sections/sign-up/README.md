a1f4cb61e483c76883a63ce134df0e636f74587e
erias04 <github.eliascerne@icloud.com>
2022-06-16 20:08:17 +0200

    create: new lib signUp in landing/sections

---
+++ libs/web/layouts/landing/sections/sign-up/README.md
@@ -0,0 +1,7 @@
+# web-layouts-landing-sections-sign-up
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test web-layouts-landing-sections-sign-up` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/web/layouts/landing/sections/sign-up/src/lib/WebLayoutsLandingSectionsSignUp.tsx
@@ -0,0 +1,16 @@
+import styles from './WebLayoutsLandingSectionsSignUp.module.css';
+
+/* eslint-disable-next-line */
+export interface WebLayoutsLandingSectionsSignUpProps {}
+
+export function WebLayoutsLandingSectionsSignUp(
+  props: WebLayoutsLandingSectionsSignUpProps
+) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WebLayoutsLandingSectionsSignUp!</h1>
+    </div>
+  );
+}
+
+export default WebLayoutsLandingSectionsSignUp;
